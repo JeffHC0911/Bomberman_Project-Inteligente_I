fb7cb72d578ee01b7a5a79fe86d2b9b730f30865
JeffHC0911 <jefferhc0911@gmail.com>
2024-09-30 10:51:40 -0500

    Image integration

--- src/view/server.py
+++ src/view/server.py
@@ -5,18 +5,18 @@ from model.agents import Bomberman, Enemy, Rock, Metal, Path
 
 def agent_portrayal(agent):
     if isinstance(agent, Bomberman):
-        portrayal = {"Shape": "circle", "Color": "blue", "Filled": "true", "Layer": 0, "r": 0.5}
+        portrayal = {"Shape": "resources/assets/bomberman.png", "Filled": "true", "Layer": 0, "r": 0.5}
     elif isinstance(agent, Enemy):
         portrayal = {"Shape": "circle", "Color": "red", "Filled": "true", "Layer": 0, "r": 0.5}
     elif isinstance(agent, Rock):
-        portrayal = {"Shape": "rect", "Color": "gray", "Filled": "true", "Layer": 0, "w": 1, "h": 1}
+        portrayal = {"Shape": "resources/assets/pared.png", "Filled": "true", "Layer": 0, "w": 1, "h": 1}
     elif isinstance(agent, Metal):
-        portrayal = {"Shape": "rect", "Color": "black", "Filled": "true", "Layer": 0, "w": 1, "h": 1}
+        portrayal = {"Shape": "resources/assets/metal.png", "Color": "black", "Filled": "true", "Layer": 0, "w": 1, "h": 1}
     elif isinstance(agent, Path):
         portrayal = {"Shape": "rect", "Color": "green", "Filled": "true", "Layer": 0, "w": 1, "h": 1}
     return portrayal
 
-grid = CanvasGrid(agent_portrayal, 7, 4, 400, 400)  # Ajusta el tamaño según tu mapa
+grid = CanvasGrid(agent_portrayal, 7, 4, 350, 250)  # Ajusta el tamaño según tu mapa
 
 server = ModularServer(
     BombermanModel, 
@@ -27,7 +27,7 @@ server = ModularServer(
         "height": 4, 
         "num_bombers": 0, 
         "num_enemies": 0, 
-        "map_file": "resources/maps/map1.txt"
+        "map_file": "resources/maps/map2.txt"
     }
 )
 server.port = 8521  # Puerto predeterminado para el servidor de Mesa
